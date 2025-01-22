5d30132036b9a55cd24182cfc59812d6c40fbaa0
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-01-22 12:12:51 +0700

    menambahkan edit.php di tamu dan menyesuaikan dengan database

---
+++ tamu/edit.php
@@ -0,0 +1,64 @@
+<?php
+#1. koneksi ke database
+include("../koneksi.php");
+
+#2. ambil id yang akan disunting
+$id = $_GET['id_tamu'];
+
+#3. mengambil semua record data berdasarkan id yang dipilih
+$ambil = "SELECT * FROM tamu WHERE id_tamu='$id_tamu'";
+
+#4. menjalankan query
+$edit = mysqli_query($koneksi,$ambil);
+
+#5. memisahkan record data berdasarkan kolom/field
+$data = mysqli_fetch_array($edit)
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Tamu</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/all.css">
+</head>
+<body>
+<?php 
+    include_once('../navbar.php');
+?>
+
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-8 m-auto">
+            <div class="card">
+            <div class="card-header">
+                <h3 class="float-start">Form Edit Data Tamu</h3>
+                
+            </div>
+            <div class="card-body">
+            <form action="update.php" method="post">
+                <input type="hidden" name="id_tamu" value="<?=$data['id_tamu']?>">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Nama Tamu</label>
+                    <input type="text" value="<?=$data['nama_tamu']?>" name="nama_tamu" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Nomor Identitas</label>
+                    <input type="text" value="<?=$data['nomor_identitas']?>" name="nomor_identitas" class="form-control" id="exampleInputPassword1">
+                </div>
+               
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/all.js"></script>
+</body>
+</html>
