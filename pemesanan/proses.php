ba5e17bded2f9a238655206c52597a2834294f3a
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-01-22 15:19:54 +0700

    memperbaiki proses.php

--- pemesanan/proses.php
+++ pemesanan/proses.php
@@ -3,24 +3,25 @@
 $tanggal_checkin = $_POST['tanggal_checkin'];
 $tanggal_checkout = $_POST['tanggal_checkout'];
 $id_tamu = $_POST['id_tamu'];
-$bukti_pembayaran = "";
 
 // Proses unggah bukti pembayaran
-if (isset($_FILES['foto']) && $_FILES['foto']['name'] != "") {
-    $bukti_pembayaran = $_FILES['foto']['name'];
+if (isset($_FILES['bukti_pembayaran']) && $_FILES['bukti_pembayaran']['name'] != "") {
+    $bukti_pembayaran = $_FILES['bukti_pembayaran']['name'];
     $target = "../uploads/" . basename($bukti_pembayaran);
 
-    if (!move_uploaded_file($_FILES['foto']['tmp_name'], $target)) {
+    if (!move_uploaded_file($_FILES['bukti_pembayaran']['tmp_name'], $target)) {
         die("Gagal mengunggah bukti pembayaran.");
     }
 }
 
 // Query simpan ke database
 $simpan = "INSERT INTO pemesanan (tanggal_checkin, tanggal_checkout, id_tamu, bukti_pembayaran) 
-           VALUES ('$tanggal_checkin', '$tanggal_checkout', '$id_tamu', '$bukti_pembayaran')";
+           VALUES ('$tanggal_checkin', '$tanggal_checkout', '$id_tamu', '$nama_foto')";
 
 // Jalankan query
 $proses = mysqli_query($koneksi, $simpan);
+
+$upload_foto = move_uploaded_file($tmp_foto,"foto/$nama_foto");
 
 // Redirect ke index
 if ($proses) {
@@ -29,3 +30,17 @@ if ($proses) {
     echo "<script>alert('Data gagal ditambahkan!'); document.location='index.php';</script>";
 }
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
