2dae54a0a6134a35be947aa7d9ddab9b25179939
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-01-22 11:31:25 +0700

    menambah dan menyesuaikan edit.php di pemesanan

--- pemesanan/edit.php
+++ pemesanan/edit.php
@@ -9,6 +9,52 @@
 </head>
 <body>
 
+    <?php 
+        include_once('../navbar.php');
+    ?>
+
+
+    
+<div class="container">
+    <div class="row mt-5">
+        <div class="col-8 m-auto">
+            <div class="card">
+            <div class="card-header">
+                <h3 class="float-start">Form Edit Data Pemesanan</h3>
+                
+            </div>
+            <div class="card-body">
+            <form action="update.php" method="post">
+                <input type="hidden" name="id" value="<?=$data['id']?>">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Tanggal Checkin</label>
+                    <input type="date" value="<?=$data['tanggal_checkin']?>" name="tanggal_checkin" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Tanggal Checkout</label>
+                    <input type="date" value="<?=$data['tanggal_checkout']?>" name="tanggal_checkout" class="form-control" id="exampleInputPassword1">
+                </div>
+                
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Nama</label>
+                    <input type="text" name="id_nama" value="<?=$data['id_nama']?>" class="form-control" id="exampleInputPassword1">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Bukti Pembayaran</label>
+                    <input type="image" name="bukti_pembayaran" value="<?=$data['bukti_pembayaran']?>" class="form-control" id="exampleInputPassword1">
+                </div>
+                
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
     <script src="../js/bootstrap.js"></script>
     <script src="../js/bootstrap.bundle.js"></script>
     <script src="../js/all.js"></script>
