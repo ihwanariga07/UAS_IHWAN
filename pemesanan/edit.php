7075599bf212ffabc18ac488fc38420aa385edc2
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-01-22 11:26:19 +0700

    menambah bootstrap di edit.php pemesanan

--- pemesanan/edit.php
+++ pemesanan/edit.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Data Dosen</title>
+    <title>Data Pemesanan</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/all.css">
 </head>
 <body>
-    
+
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+    <script src="../js/all.js"></script>
+
 </body>
 </html>
